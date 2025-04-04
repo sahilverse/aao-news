9d195f0758f2bb060fb9fdad263cf8001f03e1b7
Sahil Dahal <denenme49@gmail.com>
2025-04-04 22:10:46 +0545

    Add password utils for security

--- src/main/java/com/aaonews/utils/PasswordUtil.java
+++ src/main/java/com/aaonews/utils/PasswordUtil.java
@@ -1,2 +1,29 @@
-package com.aaonews.utils;public class PasswordUtil {
+package com.aaonews.utils;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class PasswordUtil {
+
+    /**
+     * Hashes a password using bcrypt.
+     *
+     * @param password the raw password
+     * @return the hashed password
+     */
+
+    public static String hashPassword(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt(10));
+    }
+
+    /**
+     * Compares a raw password with the hashed password.
+     *
+     * @param rawPassword the raw password
+     * @param storedHash the stored bcrypt hashed password
+     * @return true if the password matches, false otherwise
+     */
+    public static boolean comparePassword(String rawPassword, String storedHash) {
+        return BCrypt.checkpw(rawPassword, storedHash);
+    }
+
 }
