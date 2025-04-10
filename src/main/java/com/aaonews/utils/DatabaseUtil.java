41829e15a3044b6b14566a32df200ce9aca08661
Sahil Dahal <denenme49@gmail.com>
2025-04-10 13:04:20 +0545

    update DatabaseUtil

---
+++ src/main/java/com/aaonews/utils/DatabaseUtil.java
@@ -0,0 +1,53 @@
+package com.aaonews.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.io.InputStream;
+
+/**
+ * DatabaseUtil.java
+ * This class is responsible for establishing a connection to the database.
+ * It uses a db.properties file to load configuration.
+ * The class provides a method to get a connection to the database.
+ */
+public class DatabaseUtil {
+
+    private static String jdbcUrl;
+    private static String username;
+    private static String password;
+
+    static {
+        try {
+            // Load properties from db.properties
+            Properties props = new Properties();
+            InputStream input = DatabaseUtil.class.getClassLoader().getResourceAsStream("db.properties");
+            props.load(input);
+
+            String host = props.getProperty("DB_HOST");
+            String port = props.getProperty("DB_PORT");
+            String dbName = props.getProperty("DB_NAME");
+            username = props.getProperty("DB_USER");
+            password = props.getProperty("DB_PASSWORD");
+
+            jdbcUrl = String.format("jdbc:mysql://%s:%s/%s?sslmode=require", host, port, dbName);
+
+            // Load the MySQL JDBC driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Gets a connection to the MySQL database.
+     *
+     * @return Connection object
+     * @throws SQLException if a database access error occurs
+     */
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(jdbcUrl, username, password);
+    }
+}
