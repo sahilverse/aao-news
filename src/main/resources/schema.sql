e7a80fb87bc62f2a8496c37a2c83738a3e350f76
Sahil Dahal <denenme49@gmail.com>
2025-04-13 14:19:25 +0545

    Add: Publisher Related Tables

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -14,18 +14,58 @@ CREATE TABLE IF NOT EXISTS roles
 -- Users Table Definition
 CREATE TABLE IF NOT EXISTS users
 (
-    user_id        INT AUTO_INCREMENT PRIMARY KEY,
+    id             INT AUTO_INCREMENT PRIMARY KEY,
     email          VARCHAR(255) NOT NULL UNIQUE,
     username       VARCHAR(50)  NOT NULL UNIQUE,
     password       VARCHAR(255) NOT NULL,
     full_name      VARCHAR(100) NOT NULL,
-    role_id        INT UNSIGNED NOT NULL,
+    role_id        INT          NOT NULL,
+    phone_number   VARCHAR(15)  NOT NULL UNIQUE,
     email_verified BOOLEAN   DEFAULT FALSE,
     profile_image  MEDIUMBLOB, -- Profile image stored as a BLOB max size 16MB
     created_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (role_id) REFERENCES roles (id)
 ) COMMENT ='Stores user information and credentials';
+
+
+
+-- Publisher Related Tables ---------------------------------------------
+
+-- Publishers Information Table
+CREATE TABLE IF NOT EXISTS publisher_info
+(
+    publisher_id      INT PRIMARY KEY,
+    is_individual     BOOLEAN DEFAULT TRUE,
+    is_verified       BOOLEAN DEFAULT FALSE,
+    verification_date TIMESTAMP NULL,
+    FOREIGN KEY (publisher_id) REFERENCES users (id) ON DELETE CASCADE
+) COMMENT = 'Stores core info and verification status for all publishers (individuals or organizations)';
+
+-- Publisher Individual Table
+CREATE TABLE IF NOT EXISTS individual_info
+(
+    publisher_id     INT PRIMARY KEY,
+    national_id_type VARCHAR(50) NOT NULL,
+    national_id_no   VARCHAR(50) NOT NULL,
+    FOREIGN KEY (publisher_id) REFERENCES publisher_info (publisher_id) ON DELETE CASCADE
+) COMMENT = 'Stores identity verification info for individual publishers';
+
+
+-- Publisher Organization Table
+CREATE TABLE IF NOT EXISTS organization_info
+(
+    publisher_id         INT PRIMARY KEY,
+    organization_name    VARCHAR(100) NOT NULL,
+    organization_website VARCHAR(255),
+    pan_number           VARCHAR(20)  NOT NULL,
+    FOREIGN KEY (publisher_id) REFERENCES publisher_info (publisher_id) ON DELETE CASCADE
+) COMMENT = 'Stores organization-specific information for publishers who are organizations';
+
+-- END Publisher Related Tables ---------------------------------------------
+
+
+
 
 
 
