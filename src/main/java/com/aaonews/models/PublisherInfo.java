c002dea74bad05f1945eb3d14b70ede07510f043
Sahil Dahal <denenme49@gmail.com>
2025-04-14 23:11:58 +0545

    ADD: PublisherInfo Model

---
+++ src/main/java/com/aaonews/models/PublisherInfo.java
@@ -0,0 +1,55 @@
+package com.aaonews.models;
+
+import java.sql.Timestamp;
+
+
+/**
+ * PublisherInfo model class.
+ * This class represents the information of a publisher in the system.
+ */
+
+public class PublisherInfo {
+    private int publisherId;
+    private boolean isIndividual;
+    private boolean isVerified;
+    private Timestamp verificationDate;
+
+    public PublisherInfo(int publisherId, boolean isIndividual, boolean isVerified, Timestamp verificationDate) {
+        this.publisherId = publisherId;
+        this.isIndividual = isIndividual;
+        this.isVerified = isVerified;
+        this.verificationDate = verificationDate;
+    }
+
+    public int getPublisherId() {
+        return publisherId;
+    }
+
+    public void setPublisherId(int publisherId) {
+        this.publisherId = publisherId;
+    }
+
+    public boolean isIndividual() {
+        return isIndividual;
+    }
+
+    public void setIndividual(boolean individual) {
+        isIndividual = individual;
+    }
+
+    public boolean isVerified() {
+        return isVerified;
+    }
+
+    public void setVerified(boolean verified) {
+        isVerified = verified;
+    }
+
+    public Timestamp getVerificationDate() {
+        return verificationDate;
+    }
+
+    public void setVerificationDate(Timestamp verificationDate) {
+        this.verificationDate = verificationDate;
+    }
+}
