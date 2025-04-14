028309a1bbdae3cacfc7b6e6a4a8615b1cbbd5cf
Sahil Dahal <denenme49@gmail.com>
2025-04-14 21:23:51 +0545

    Update: Sql Schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -31,7 +31,7 @@ CREATE TABLE IF NOT EXISTS users
     id             INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     email          VARCHAR(255) NOT NULL UNIQUE,
     username       VARCHAR(50)  NOT NULL UNIQUE,
-    password_hash  VARCHAR(255) NOT NULL,
+    password  VARCHAR(255) NOT NULL,
     full_name      VARCHAR(100) NOT NULL,
     role_id        INT UNSIGNED NOT NULL,
     phone_number   VARCHAR(15)  NULL UNIQUE,
