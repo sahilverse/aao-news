ea8aaf68cfaa73e09605420dd05dbf4b6ebf26e2
Sahil Dahal <denenme49@gmail.com>
2025-04-15 08:51:36 +0545

    ADD: ArticleLike Model

---
+++ src/main/java/com/aaonews/models/ArticleLike.java
@@ -0,0 +1,50 @@
+package com.aaonews.models;
+
+import java.sql.Timestamp;
+
+public class ArticleLike {
+    private int id;
+    private int articleId;
+    private int userId;
+    private Timestamp createdAt;
+
+    public ArticleLike(int id, int articleId, int userId, Timestamp createdAt) {
+        this(articleId, userId);
+        this.id = id;
+        this.createdAt = createdAt;
+    }
+
+    public ArticleLike(int articleId, int userId) {
+        this.articleId = articleId;
+        this.userId = userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+    public int getArticleId() {
+        return articleId;
+    }
+
+    public void setArticleId(int articleId) {
+        this.articleId = articleId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+}
