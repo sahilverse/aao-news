a6d39ad5be8e8ca785d5a8cd4f12b9f56b0aa2f8
Raghav <raghavdahal686@gmail.com>
2025-04-17 22:14:22 +0545

    created session

--- src/main/java/com/aaonews/controllers/LoginServlet.java
+++ src/main/java/com/aaonews/controllers/LoginServlet.java
@@ -7,6 +7,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 
 import java.io.IOException;
@@ -20,5 +21,18 @@ public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/login.jsp");
         dispatcher.forward(request, response);
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+//    session created==> tara aba yo email ra pwd database ma hernu paro haina? database ta hamile banakei
+//        xaina ta. schema matra xa ta.
+        HttpSession session = request.getSession();
+
+
+
+        System.out.println(email + " " + password);
     }
 }
